e70371dff5190ca0c21bad0c135d1eaaca8db559
giorgi <sharmazanashvilig@gmail.com>
2022-02-11 17:26:57 +0400

    Feedbacks update

--- users/models.py
+++ users/models.py
@@ -23,4 +23,4 @@ class FriendRequest(models.Model):
     from_user = models.ForeignKey(User, related_name="from_user", on_delete=models.CASCADE)
     to_user = models.ForeignKey(User, related_name="to_user", on_delete=models.CASCADE)
 
-
+    
